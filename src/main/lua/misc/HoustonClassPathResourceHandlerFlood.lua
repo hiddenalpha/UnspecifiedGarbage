cb03bcc4e837ab252bbe59a52d5f632c2d80e23a
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2022-11-30 18:41:23 +0100

    Try to go parallel. But does not work somehow :(

--- src/main/lua/misc/HoustonClassPathResourceHandlerFlood.lua
+++ src/main/lua/misc/HoustonClassPathResourceHandlerFlood.lua
@@ -7,19 +7,11 @@
   but no idea yet why sometimes we see those exceptions while classpath
   resource requests.
 
-  20221122 issue with scriptlee 0.0.5-23-gc70b3c1-G :
-
-RuntimeError: [string "C:/Users/fankhauseand/projects/UnspecifiedGar..."]:72: Unknown error (@src/main/c/scriptlee/lua_http.c:1848)
-stack traceback:
-        [C]: in method 'closeSnk'
-        [string "C:/Users/fankhauseand/projects/UnspecifiedGar..."]:72: in field 'run'
-        [string "C:/Users/fankhauseand/projects/UnspecifiedGar..."]:91: in function <[string "C:/Users/fankhauseand/projects/UnspecifiedGar..."]:85>
-        [C]: in ?
-
   ]====================================================================]
 
 local AF_INET = assert(require("scriptlee").posix.AF_INET)
 local SOCK_STREAM = assert(require("scriptlee").posix.SOCK_STREAM)
+local async = assert(require("scriptlee").reactor.async)
 local inaddrOfHostname = require('scriptlee').posix.inaddrOfHostname
 local newHttpClient = assert(require("scriptlee").newHttpClient)
 local objectSeal = assert(require("scriptlee").objectSeal)
@@ -36,6 +28,7 @@ function mod.newSocketMgr( app )
     }
     local m = {
         openSock = function(t, opts)
+            stdlog:write("openSock()\n")
             assert(opts.useTLS == false)
             local host, port = opts.host, opts.port
             local socktsForThisHost = t.socktsIdle[host.."\t"..port]
@@ -44,31 +37,34 @@ function mod.newSocketMgr( app )
                 s = table.remove(socktsForThisHost, #socktsForThisHost)
             end
             if not s then -- create a new one
+                stdlog:write("s = socket(...)\n")
                 local inaddr = inaddrOfHostname(host)
                 s = socket(AF_INET, SOCK_STREAM, 0)
                 s:connect(host, port)
             else
                 -- s is already set to an old socket ready-to-reuse
+                stdlog:write("s = getExisting()\n")
             end
             return objectSeal{
-                orig = assert(s),
+                base = assert(s),
                 host = assert(host),
                 port = assert(port),
                 -- exposed API:
-                write = function(t, buf, beg, len) t.orig:write(buf, beg, len) end,
-                flush = function(t) t.orig:flush() end,
-                read = function(t, a, b, c, d) return t.orig:read() end,
+                write = function(t, buf, beg, len) t.base:write(buf, beg, len) end,
+                flush = function(t) t.base:flush() end,
+                read = function(t, a, b, c, d) return t.base:read() end,
             }
         end,
         releaseSock = function(t, s)
+            stdlog:write("releaseSock()\n")
             local socktsForThisHost = t.socktsIdle[s.host.."\t"..s.port]
             if not socktsForThisHost then
                 socktsForThisHost = {}
                 t.socktsIdle[s.host.."\t"..s.port] = socktsForThisHost
             end
-            table.insert(socktsForThisHost, s.orig)
+            table.insert(socktsForThisHost, s.base)
         end,
-        --closeSock = function(t, s) s.orig:close() end,
+        closeSock = function(t, s) stdlog:write("closeSock("..tostring(s.base)..")\n") s.base:close() end,
     }
     m.__index = m
     return setmetatable(t, m)
@@ -77,16 +73,20 @@ end
 
 function mod.run( app )
     local httpClient = app.httpClient
-    for i=0, 3 do
-        httpClient:request{
-            --cls = ,
-            host = "127.0.0.1", port = 7013,
-            method = "GET", url = "/houston/server/apps/rest-editor/app/css/editor.css",
-            --hdrs = ,
-            --onRspHdr = ,
-            --onRspChunk = ,
-            onRspEnd = function() stdlog:write("onRspEnd()\n") end,
-        }:closeSnk()
+    local method, url = "GET", "/houston/server/apps/rest-editor/app/css/editor.css"
+    for i=1, 64 do
+        async(function( i )
+            stdlog:write(""..method.." "..url.."  (i "..i..")\n")
+            httpClient:request{
+                --cls = ,
+                host = "127.0.0.1", port = 7013,
+                method = method, url = url,
+                --hdrs = ,
+                --onRspHdr = ,
+                --onRspChunk = ,
+                onRspEnd = function() stdlog:write("onRspEnd()\n") end,
+            }:closeSnk()
+        end, i)
     end
 end
 
