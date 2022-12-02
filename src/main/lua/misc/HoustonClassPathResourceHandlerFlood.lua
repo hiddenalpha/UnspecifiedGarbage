bb50c133ed7d76f0434381b233d2ba5300e414fb
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2022-12-02 21:54:16 +0100

    DUMP

--- src/main/lua/misc/HoustonClassPathResourceHandlerFlood.lua
+++ src/main/lua/misc/HoustonClassPathResourceHandlerFlood.lua
@@ -15,6 +15,7 @@ local async = assert(require("scriptlee").reactor.async)
 local inaddrOfHostname = require('scriptlee').posix.inaddrOfHostname
 local newHttpClient = assert(require("scriptlee").newHttpClient)
 local objectSeal = assert(require("scriptlee").objectSeal)
+local sleep = assert(require("scriptlee").posix.sleep)
 local socket = assert(require("scriptlee").posix.socket)
 local startOrExecute = assert(require("scriptlee").reactor.startOrExecute)
 
@@ -24,26 +25,30 @@ local stdinn, stdout, stdlog = io.stdin, io.stdout, io.stderr
 
 function mod.newSocketMgr( app )
     local t = {
+        numWorking = 0,
         socktsIdle = {},
     }
     local m = {
         openSock = function(t, opts)
-            stdlog:write("openSock()\n")
+            --stdlog:write("openSock()\n")
             assert(opts.useTLS == false)
             local host, port = opts.host, opts.port
+            -- Limit max parallel connections
+            while t.numWorking >= 4 do sleep(0.001) end
+            t.numWorking = t.numWorking + 1
             local socktsForThisHost = t.socktsIdle[host.."\t"..port]
             local s = false
             if socktsForThisHost and #socktsForThisHost > 0 then
                 s = table.remove(socktsForThisHost, #socktsForThisHost)
             end
             if not s then -- create a new one
-                stdlog:write("s = socket(...)\n")
+                --stdlog:write("s = socket(...)\n")
                 local inaddr = inaddrOfHostname(host)
                 s = socket(AF_INET, SOCK_STREAM, 0)
                 s:connect(host, port)
             else
                 -- s is already set to an old socket ready-to-reuse
-                stdlog:write("s = getExisting()\n")
+                --stdlog:write("s = getExisting()\n")
             end
             return objectSeal{
                 base = assert(s),
@@ -56,7 +61,8 @@ function mod.newSocketMgr( app )
             }
         end,
         releaseSock = function(t, s)
-            stdlog:write("releaseSock()\n")
+            --stdlog:write("releaseSock()\n")
+            t.numWorking = t.numWorking - 1
             local socktsForThisHost = t.socktsIdle[s.host.."\t"..s.port]
             if not socktsForThisHost then
                 socktsForThisHost = {}
@@ -64,7 +70,10 @@ function mod.newSocketMgr( app )
             end
             table.insert(socktsForThisHost, s.base)
         end,
-        closeSock = function(t, s) stdlog:write("closeSock("..tostring(s.base)..")\n") s.base:close() end,
+        closeSock = function(t, s)
+            stdlog:write("closeSock("..tostring(s.base)..")\n")
+            s.base:close()
+        end,
     }
     m.__index = m
     return setmetatable(t, m)
@@ -74,17 +83,22 @@ end
 function mod.run( app )
     local httpClient = app.httpClient
     local method, url = "GET", "/houston/server/apps/rest-editor/app/css/editor.css"
-    for i=1, 64 do
+    for i=1, 1024 do
         async(function( i )
-            stdlog:write(""..method.." "..url.."  (i "..i..")\n")
+            --stdlog:write(""..method.." "..url.."  (i "..i..")\n")
             httpClient:request{
                 --cls = ,
-                host = "127.0.0.1", port = 7013,
+                host = "127.0.0.1", port = 7012,
+                --host = "127.0.0.1", port = 7013,
                 method = method, url = url,
                 --hdrs = ,
-                --onRspHdr = ,
+                --onRspHdr = function( rspHdr, cls )
+                --    stdlog:write( rspHdr.proto .." ".. rspHdr.status .." ".. rspHdr.phrase .."\n")
+                --end,
                 --onRspChunk = ,
-                onRspEnd = function() stdlog:write("onRspEnd()\n") end,
+                --onRspEnd = function()
+                --    stdlog:write("onRspEnd()\n")
+                --end,
             }:closeSnk()
         end, i)
     end
