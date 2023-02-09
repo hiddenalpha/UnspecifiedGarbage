a34ad40d2c4e1f783c92cba66e9574fa1c596d01
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2023-02-09 13:00:56 +0100

    Tinker for an observe teddie script

---
+++ src/main/lua/misc/ObserveHashContainers.lua
@@ -0,0 +1,62 @@
+
+local SL = require("scriptlee")
+local newShellcmd = SL.newShellcmd
+local objectSeal = SL.objectSeal
+local sleep = SL.posix.sleep
+local startOrExecute = SL.reactor.startOrExecute
+SL = nil
+
+local log = io.stderr
+local mod = {}
+
+
+function mod.printHelp()
+    io.stdout:write("\n"
+        .."  TODO write help page\n"
+        .."\n"
+        --.."  Options:\n"
+        --.."\n"
+        .."\n")
+end
+
+
+function mod.parseArgs( app )
+    local iA = 0
+    while true do iA = iA + 1
+        local arg = _ENV.arg[iA]
+        if not arg then
+            break
+        elseif arg == "--help" then
+            mod.printHelp() return -1
+        else
+            log:write("Unexpected arg: "..tostring(arg).."\n") return -1
+        end
+    end
+    return 0
+end
+
+
+function mod.run( app )
+    local cmd = newShellcmd{
+        cmd = "printf 'guguseli\n'",
+    }
+    cmd:start()
+    cmd:closeSnk()
+    while true do
+        local exit, signal = cmd:join(1000)
+        log:write("exit:   "..tostring(exit).."\n")
+        log:write("signal: "..tostring(signal).."\n")
+        sleep(1)
+    end
+    log:write("TODO not impl yet\n")
+end
+
+
+function mod.main()
+    local app = objectSeal{}
+    if mod.parseArgs(app) ~= 0 then os.exit(1) end
+    mod.run(app)
+end
+
+
+startOrExecute(nil, mod.main)
