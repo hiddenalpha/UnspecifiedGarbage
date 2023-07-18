91b9a5592007fc6a26f67b3c5c6cfd81ca84eb81
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2023-07-18 22:04:38 +0200

    (firefox-plugin) Cleanup.

--- src/main/firefox/gaga-plugin/main.js
+++ src/main/firefox/gaga-plugin/main.js
@@ -10,8 +10,10 @@
     var STATUS_RUNNING = 2;
     var STATUS_DONE = 3;
     var STATUS_OBSOLETE = 4;
+    var NOOP = function(){};
+    var LOGERR = console.error.bind(console);
     var N = null;
-    var setTimeout, logErrors;
+    var setTimeout, logErrors, LOGDBG;
 
 
     function main(){
@@ -24,9 +26,11 @@
         if( NDEBUG ){
             setTimeout = window.setTimeout;
             logErrors = function(app, fn){ fn(arguments.slice(2)); }
+            LOGDBG = NOOP;
         }else{ /* fix broken tooling */
             setTimeout = setTimeoutWithCatch.bind(0, app);
             logErrors = logErrorsImpl.bind(N, app);
+            LOGDBG = console.debug.bind(console);
         }
         document.addEventListener("DOMContentLoaded", logErrors.bind(N, onDOMContentLoaded, app));
     }
@@ -34,53 +38,50 @@
 
     function onDOMContentLoaded( app ){
         cleanupClutter(app);
-        tryStuffWithObserver(app);
+        attachDomObserver(app);
     }
 
 
-    function tryStuffWithObserver( app ){
-        var elem = getBloatyChecklistBtn(app);
-        if( !elem ){ throw Error("whops"); }
-        new MutationObserver(onFooHasChanged.bind(N, app))
-            .observe(document, { subtree:true, childList:true, attributes:true, characterData:true });
+    function attachDomObserver( app ){
+        new MutationObserver(onDomhasChangedSomehow.bind(N, app))
+            .observe(document, { subtree:true, childList:true, attributes:true });
     }
 
 
-    function onFooHasChanged( app, changes, mutationObserver ){
+    function onDomhasChangedSomehow( app, changes, mutationObserver ){
         var nowEpochMs = Date.now();
-        if( (app.lastClickEpochMs + 2000) > nowEpochMs ){ console.debug("ignore, likely was triggered by user."); return; }
+        if( (app.lastClickEpochMs + 2000) > nowEpochMs ){
+            LOGDBG("ignore, likely triggered by user.");
+            return; }
         var needsReEval = false;
         for( var change of changes ){
+            if( change.target.nodeName != "BUTTON" ) continue;
             var isAriaExpanded = (change.attributeName == "aria-expanded");
             var isChildAdded = (change.addedNodes.length > 0);
             var isChildRemoved = (change.removedNodes.length > 0);
             var isChildAddedOrRemoved = isChildAdded || isChildRemoved;
             if( !isAriaExpanded && !isChildAddedOrRemoved ) continue;
-            var isButton = (change.target.nodeName == "BUTTON");
-            if( !isButton ) continue;
             if( isAriaExpanded ){
-                console.debug("Suspicious, isExpanded: ", change.target);
+                LOGDBG("Suspicious, isExpanded: ", change.target);
                 needsReEval = true; break;
             }
             if( !isChildAddedOrRemoved ) continue;
             var isBloatyChecklistBtnStillThere = document.body.contains(getBloatyChecklistBtn(app));
             if( !isBloatyChecklistBtnStillThere ){
-                console.debug("Suspicious, btn lost: ");
+                LOGDBG("Suspicious, btn lost");
                 needsReEval = true; break;
             }
             var isBloatyDevelopmentBtnStillThere = document.body.contains(getBloatyDevelopmentBtn(app));
             if( !isBloatyDevelopmentBtnStillThere ){
-                console.debug("Suspicious, btn lost: ");
+                LOGDBG("Suspicious, btn lost");
                 needsReEval = true; break;
             }
         }
         if( needsReEval ){
-            console.debug("Change detected! Eval again");
+            LOGDBG("Change detected! Eval again");
             app.ui.bloatyChecklistBtn = null;
             app.ui.bloatyDevelopmentBtn = null;
             setTimeout(cleanupClutter, 42, app);
-        //}else{
-        //    console.log("Changes seen, but nothing suspicious.");
         }
     }
 
@@ -98,37 +99,48 @@
 
 
     function getBloatyChecklistBtn( app ){
-        if( ! app.ui.bloatyChecklistBtn ){
-            var btn = app.ui.bloatyChecklistBtn = fetchUiRefOrNull(app, document, 'button[aria-label="Checklists"]');
-            btn.addEventListener("mousedown", logErrors.bind(N, function(app){app.lastClickEpochMs = Date.now();}, app));
+        if( !app.ui.bloatyChecklistBtn ){
+            var btn = fetchUiRefOrNull(app, document, 'button[aria-label="Checklists"]');
+            if( btn ){
+                btn.addEventListener("mousedown", logErrors.bind(N, setLastClickTimeToNow, app));
+                app.ui.bloatyChecklistBtn = btn;
+            }
         }
         return app.ui.bloatyChecklistBtn;
     }
 
 
     function getBloatyDevelopmentBtn( app ){
-        if( ! app.ui.bloatyDevelopmentBtn ){
-            var btn = app.ui.bloatyDevelopmentBtn = fetchUiRefOrNull(app, document, 'button[aria-label="Development"]');
-            btn.addEventListener("mousedown", logErrors.bind(N, function(app){app.lastClickEpochMs = Date.now();}, app));
+        if( !app.ui.bloatyDevelopmentBtn ){
+            var btn = fetchUiRefOrNull(app, document, 'button[aria-label="Development"]');
+            if( btn ){
+                btn.addEventListener("mousedown", logErrors.bind(N, setLastClickTimeToNow, app));
+                app.ui.bloatyDevelopmentBtn = btn;
+            }
         }
         return app.ui.bloatyDevelopmentBtn;
     }
 
 
+    function setLastClickTimeToNow( app ){ app.lastClickEpochMs = Date.now(); }
+
+
     function hideBloatyChecklist( app ){
-        if( app.bloatyChecklistDone == STATUS_DONE ){ console.debug("bloatyChecklistBtn now hidden", app.ui.bloatyChecklistBtn); return; }
-        app.bloatyChecklistDone == STATUS_RUNNING;
+        if( app.bloatyChecklistDone == STATUS_DONE ){
+            LOGDBG("bloatyChecklistBtn now hidden", app.ui.bloatyChecklistBtn);
+            return; }
+        app.bloatyChecklistDone = STATUS_RUNNING;
         var btn = getBloatyChecklistBtn(app);
         do{
-            if( ! btn ){ console.debug("Button not found. DOM maybe not yet ready?"); break; }
+            if( ! btn ){ LOGDBG("Button not found. DOM maybe not yet ready?"); break; }
             var isExpanded = isAriaBtnExpanded(app, btn);
             if( isExpanded === true ){
-                console.debug("bloatyChecklistBtn.click()");
+                LOGDBG("bloatyChecklistBtn.click()");
                 btn.click();
             }else if( isExpanded === false ){
                 app.bloatyChecklistDone = STATUS_DONE;
             }else{
-                console.debug("Neither true nor false: "+ typeof(isExpanded) +" "+ isExpanded);
+                throw Error("Neither true nor false: "+ typeof(isExpanded) +" "+ isExpanded);
             }
         }while(0);
         /*try later*/
@@ -137,19 +149,21 @@
 
 
     function hideBloatyDevelopment( app ){
-        if( app.bloatyDevelopmentDone == STATUS_DONE ){ console.debug("bloatyDevelopmentBtn now hidden", app.ui.bloatyDevelopmentBtn); return; }
-        app.bloatyDevelopmentDone == STATUS_RUNNING;
+        if( app.bloatyDevelopmentDone == STATUS_DONE ){
+            LOGDBG("bloatyDevelopmentBtn now hidden");
+            return; }
+        app.bloatyDevelopmentDone = STATUS_RUNNING;
         var btn = getBloatyDevelopmentBtn(app);
         do{
-            if( ! btn ){ console.debug("Button not found. DOM maybe not yet ready?"); break; }
+            if( !btn ){ LOGDBG("Button not found. DOM maybe not yet ready?"); break; }
             var isExpanded = isAriaBtnExpanded(app, btn);
             if( isExpanded === true ){
-                console.debug("bloatyDevelopmentBtn.click()");
+                LOGDBG("bloatyDevelopmentBtn.click()");
                 btn.click();
             }else if( isExpanded === false ){
                 app.bloatyDevelopmentDone = STATUS_DONE;
             }else{
-                console.debug("Neither true nor false: "+ typeof(isExpanded) +" "+ isExpanded);
+                throw Error("Neither true nor false: "+ typeof(isExpanded) +" "+ isExpanded);
             }
         }while(0);
         /*try later*/
@@ -164,33 +178,24 @@
         }else if( value === "false" ){
             return false;
         }else{
-            throw Error("btn.attrib[aria-expand] is '"+ value +"'");
+            throw Error("btn[aria-expand] is '"+ value +"'");
         }
     }
 
 
     function fetchUiRefOrNull( app, searchRoot, query ){
         var elems = searchRoot.querySelectorAll(query);
-        if( elems.length > 1 ){ console.error(Error("Not unique: "+ query)); return null; }
+        if( elems.length > 1 ){ throw Error("Not unique: "+ query); }
         if( elems.length !== 1 ){ return null; }
         return elems[0];
     }
 
 
     function setTimeoutWithCatch( app, func, ms, a1, a2, a3, a4, a5, a6 ){
-        if( NDEBUG ){ console.error("Why is this code in use?"); return; }
-        if( typeof(app) != "object" ){ console.error("E_20230718192813 ", app); return; }
-        if( typeof(func) != "function" ){ console.error("E_20230718192821", func); return; }
-        if( typeof(ms) != "number" ){ console.error("E_20230718192830", ms); return; }
-        var oldStack = Error();
+        if( typeof(app) != "object" ){ LOGERR("E_20230718192813 ", app); return; }
+        if( typeof(func) != "function" ){ LOGERR("E_20230718192821", func); return; }
+        if( typeof(ms) != "number" ){ LOGERR("E_20230718192830", ms); return; }
         window.setTimeout(logErrors, ms, func, a1, a2, a3, a4, a5, a6);
-        //window.setTimeout(function(){
-        //    try{
-        //        func(a1, a2, a3, a4, a5, a6);
-        //    }catch( ex ){
-        //        console.error(ex, oldStack); throw ex;
-        //    }
-        //}, ms);
     }
 
 
@@ -198,7 +203,7 @@
         try{
             func(a1, a2, a3, a4, a5, a6, a7, a8, a9);
         }catch( ex ){
-            console.error(ex);
+            LOGERR(ex);
         }
     }
 
