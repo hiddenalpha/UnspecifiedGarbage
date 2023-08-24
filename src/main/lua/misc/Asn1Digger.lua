d11ecf06af2ad4d011a3d7e83323e435e575e268
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2023-08-24 15:49:05 +0200

    (Asn1Digger) Cleanup. Add a gpg guess.

--- src/main/lua/misc/Asn1Digger.lua
+++ src/main/lua/misc/Asn1Digger.lua
@@ -12,6 +12,9 @@ function mod.printHelp()
         .."  \n"
         .."      -c <num>\n"
         .."          Number of columns to use for hex-dumps. Defaults to 16.\n"
+        .."  \n"
+        .."      --gpg\n"
+        .."          Assume GPG and print additional info for it.\n"
         .."  \n")
 end
 
@@ -32,6 +35,8 @@ function mod.parseArgs( app )
                 return -1
             end
             app.hexCols = tonumber(app.hexCols)
+        elseif arg == "--gpg" then
+            app.assumeGPG = true
         else
             log:write("EINVAL: ".. arg .."\n")
             return -1
@@ -66,34 +71,48 @@ function mod.state_value( app )
     assert(type(app.len) == "number", app.len)
     local val = app.src:read(app.len)
     assert(val, val)
-    if app.type == 0x99 and app.len == 1 and val:byte(1) == 0x0D then
-        app.dst:write(string.format("- Certificate (t=0x%02X, l=0x01, v=0x0D)\n", app.type))
-    elseif app.type == 0x04 then
-        app.dst:write(string.format("- OctetStream (t=0x%02X, l=%d):", app.type, app.len))
-        local i = 0 while i < val:len() do i = i + 1
-            if (i-1) % 32 == 0 then app.dst:write("\n  ") end
-            app.dst:write(string.format(" %02X", val:byte(i)))
-        end
-        app.dst:write("\n")
-    -- elseif app.type == 0x2D then
-    else
-        app.dst:write(string.format("Type 0x%02X (%d), length %d:", app.type, app.type, app.len))
-        local line = ""
-        local i = 0 while i < val:len() do i = i + 1
-            if (i-1) % app.hexCols == 0 then app.dst:write(string.format("%s\n  %08X:", line, i-1)); line = "  " end
-            local char = val:byte(i)
-            app.dst:write(string.format(" %02X", char))
-            local isPrintable = false
-                or (char >= 0x20 and char <= 0x7E)
-            line = line .. (isPrintable and string.char(char) or ".")
-        end
-        if line:len() > 0 then
-            i = i % app.hexCols
-            while i < app.hexCols do app.dst:write("   "); i = i + 1 end
-            app.dst:write(line)
-        end
-        app.dst:write("\n")
+    local typeName
+    if app.type == 0x00 then typeName = "EndOfContent" end
+    if app.type == 0x02 then typeName = "integer" end
+    if app.type == 0x04 then typeName = "octet string" end
+    if app.type == 0x0C then typeName = "utf8 string" end
+    if not typeName then
+        local tagClass = false
+            or ((app.type & 0xC0) == 0) and "Universal"
+            or ((app.type & 0x40) == 0) and "Application"
+            or ((app.type & 0x80) == 0) and "Context-specifc"
+            or "Private"
+        local primOrConstr = ((app.type & 0x20) == 0)and"primitive"or"constructed"
+        local isLongType = (app.type & 0x1F == 0x1F)
+        local fmt = (isLongType) and("%s, %s, %s") or("subType 0x%02X, %s, %s")
+        typeName = string.format(fmt,
+            isLongType and("LongType") or(app.type & 0x1F),
+            tagClass,
+            primOrConstr)
     end
+    app.dst:write(string.format("ASN.1 type 0x%02X, len %d (%s), value:", app.type, app.len, typeName))
+    if app.assumeGPG then
+        if app.type == 0x95 then
+            app.dst:write(string.format("\nGPG secret key packet, version %d", val:byte(2)))
+        elseif app.type == 0x99 and app.len == 1 and val:byte(1) == 0x0D then
+            app.dst:write("\nGPG certificate")
+        end
+    end
+    local line = ""
+    local i = 0 while i < val:len() do i = i + 1
+        if (i-1) % app.hexCols == 0 then app.dst:write(string.format("%s\n  %08X:", line, i-1)); line = "  " end
+        local char = val:byte(i)
+        app.dst:write(string.format(" %02X", char))
+        local isPrintable = false
+            or (char >= 0x20 and char <= 0x7E)
+        line = line .. (isPrintable and string.char(char) or ".")
+    end
+    if line:len() > 0 then
+        i = i % app.hexCols
+        while i < app.hexCols do app.dst:write("   "); i = i + 1 end
+        app.dst:write(line)
+    end
+    app.dst:write("\n")
     app.funcToCall = mod.state_type
 end
 
@@ -110,6 +129,8 @@ end
 
 function mod.main()
     local app = {
+        isHelp = false,
+        assumeGPG = false,
         src = false,
         dst = false,
         isInnEof = false,
