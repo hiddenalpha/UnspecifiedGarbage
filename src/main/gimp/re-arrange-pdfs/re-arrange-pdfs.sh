993f54e0d0a6c04a581256d3ff9d216418187336
andreas tux-book <https://hiddenalpha.ch/slnk/id/1:c0n74ct>
2023-12-31 17:50:46 +0100

    (gimp script) move new files to separate dir

--- src/main/gimp/re-arrange-pdfs/re-arrange-pdfs.sh
+++ src/main/gimp/re-arrange-pdfs/re-arrange-pdfs.sh
@@ -14,12 +14,11 @@ true \
     all-pages.pdf \
     cat 1 3 5 7 8 9 11 12 13 14 15 16 17 18 19 20 21 22 23 24 \
     output pages-with-content.pdf \
-  && if test ! -d pages-with-content-img; then true \
-    && mkdir -p pages-with-content-img \
-    && pdfimages -all  pages-with-content.pdf  pages-with-content-img/img \
-    ;fi; true \
+  && mkdir pages-with-content-img \
+  && pdfimages -all  pages-with-content.pdf  pages-with-content-img/img \
   && X=pZZNc9owEIbv+RVbn+RmBBZp0zLpxdM4CR0KGeLkrtiLUGrLLhIk/feVjS2HTCeHSoyAtfdZ7cc7BlKg+QjkBOwiUqkFL1EDKaQ27bVmBWNT1uMSpdJjnXHFxjUXqOmzNBuaVcqgMlSWYmw3jaJo9FSL4H9p5kVPvOgzL/qTF/3Ziz73or940V+96KkPzby0xry0xry0xry0xry0xry0xry0xry0xry0xl5pLQyPHnkQdXa1M7dKONNy7nu+5c/8sRh8Zf4ClHVGpgxM2jvNJjmupUIgZiP1TP/YaRNf7bJfUolLXqp5VdVg8T7QGsi3xgYbJQTyiEKqrkii0XyAPk2izKZ17B/VXR3Oz+ZLMr4FspYF0qcaBS0qngNzIbrP8A3pqjvgQpa17ZztJhVoKM+M3CMt+B/cNoe8olvPnqYF7rHQQ7Sb2V26vF7FP+lDPL9P3OSi0aR5mwJcxfO7BODCJlbvhh8dNorgaF2A4GXJjyO0Xi6CHd4Q4jjBQylWEnvcWkmoHF8wb9v1fbl4SFYpvZylN8mKLpaLxF27jedJmib0OlkkqzhN4GzSnZau7hMIguMeduIh2mztoCmva1S5a3xAm0xGtRLB0L3aepq3RKD53prQzBBMBUEXecDa6dpI1HpiM1xbiOt5l0YvZZja3b3+1ZQc7X8AbKfa331HteS01R/rcgmd4t9h2mMPR/7eSdPY4clfwj6IxA== \
   && echo $X|base64 -d|inflate| gimp -nidfsc -b - \
-  && echo DONE \
+  && mkdir pages-image-adjusted \
+  && mv -t pages-image-adjusted/. pages-with-content-img/*gimp.png \
 
 
