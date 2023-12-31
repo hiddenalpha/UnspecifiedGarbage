7d4fdc5c2112560bec5ce40b334bfac7fa2b6d4b
andreas tux-book <https://hiddenalpha.ch/slnk/id/1:c0n74ct>
2023-12-31 17:45:05 +0100

    Fix double-call in loop

--- src/main/gimp/re-arrange-pdfs/re-arrange-pdfs.sh
+++ src/main/gimp/re-arrange-pdfs/re-arrange-pdfs.sh
@@ -17,8 +17,8 @@ true \
   && if test ! -d pages-with-content-img; then true \
     && mkdir -p pages-with-content-img \
     && pdfimages -all  pages-with-content.pdf  pages-with-content-img/img \
-    ;fi \
-  && X=pZVNc9owEIbv+RVbn+R2BBZJP5i0B0+jJHQoZBwnd9UWQqktq0iQ9N9XNrKBdiaHSozAK/aR1vu+Y6OK27eAzsANJJVasJobQJU0tltrRzS2tR7XXCozNgVTZKyZ4AY/S7vGRaMsVxbLWozdxEmSjJ60iP6XJkH0JIg+D6Ivguj3QfSHIPpjEP0piJ6G0CTIayTIayTIayTIayTIayTIayTIayTIayTIa+TIa3F88siDxMfN1t4pMYSOG67LDXtmP6pDrixfABMfFMrCRXvdTlTylVQckF1LMzPftsam19vip1TiitVq3jQaHO1RvZEOPsSG2zfQF4aUXbf/9QsmPsly9aGCbQCtZMXxk+YCVw0rgQwb+N/4hBvuZQ8LWWvXJ9c7LLjFrLByx3HFfvNNe8TAdnk9iyu+45U57HU7u8+XN1n6HT+m8wfqVUpGk/ZrCnCdzu8pwKUrSW/71wsZJXAyLkGwumbHdJcz0E6iHj8ua1++E33HN050VfIXXnYN+rpcPNIsx1ez/JZmeLFc0GHtLp3TPKf4hi5oluYUzif+pDx7oBBFx13z5kDGOsUEZlpzVQ6NjnBbx0grEcUnyv6VHxm2cyG0ioFtIPL79lCnpNsFuzzeCuluYeixL6G3KUzd9J9/m1Fy93bnnYLesStAn790dnJ+jV/1J3rX5ZG9+J2rX8nuTt+f/WsrbRvHZ38AVrSGVg== \
+    ;fi; true \
+  && X=pZZNc9owEIbv+RVbn+RmBBZp0zLpxdM4CR0KGeLkrtiLUGrLLhIk/feVjS2HTCeHSoyAtfdZ7cc7BlKg+QjkBOwiUqkFL1EDKaQ27bVmBWNT1uMSpdJjnXHFxjUXqOmzNBuaVcqgMlSWYmw3jaJo9FSL4H9p5kVPvOgzL/qTF/3Ziz73or940V+96KkPzby0xry0xry0xry0xry0xry0xry0xry0xry0xl5pLQyPHnkQdXa1M7dKONNy7nu+5c/8sRh8Zf4ClHVGpgxM2jvNJjmupUIgZiP1TP/YaRNf7bJfUolLXqp5VdVg8T7QGsi3xgYbJQTyiEKqrkii0XyAPk2izKZ17B/VXR3Oz+ZLMr4FspYF0qcaBS0qngNzIbrP8A3pqjvgQpa17ZztJhVoKM+M3CMt+B/cNoe8olvPnqYF7rHQQ7Sb2V26vF7FP+lDPL9P3OSi0aR5mwJcxfO7BODCJlbvhh8dNorgaF2A4GXJjyO0Xi6CHd4Q4jjBQylWEnvcWkmoHF8wb9v1fbl4SFYpvZylN8mKLpaLxF27jedJmib0OlkkqzhN4GzSnZau7hMIguMeduIh2mztoCmva1S5a3xAm0xGtRLB0L3aepq3RKD53prQzBBMBUEXecDa6dpI1HpiM1xbiOt5l0YvZZja3b3+1ZQc7X8AbKfa331HteS01R/rcgmd4t9h2mMPR/7eSdPY4clfwj6IxA== \
   && echo $X|base64 -d|inflate| gimp -nidfsc -b - \
   && echo DONE \
 
