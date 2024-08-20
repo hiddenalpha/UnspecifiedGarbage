fafc341d9d46be63f2fe81031a2c60d528d4a5b8
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2024-08-20 03:11:21 +0200

    Reword c debugger msg.

--- src/main/c/common/BREAK.h
+++ src/main/c/common/BREAK.h
@@ -1,6 +1,6 @@
 
 #define BREAK() do{ \
-    LOGDBG("Hardcoded breakpoint @ %s:%d\n", __FILE__, __LINE__); \
+    LOGDBG("SIGTRAP @ %s:%d\n", __FILE__, __LINE__); \
     __asm__("int $3; nop"); \
 }while(0)
 
