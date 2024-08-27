efad1586ffcaf9e52972376777044d4625f2878d
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2024-08-27 19:03:13 +0200

    Lazlar hostname hack. Java getAsHexStr() helper func.

--- src/main/java/ch/hiddenalpha/unspecifiedgarbage/format/FormatUtils.java
+++ src/main/java/ch/hiddenalpha/unspecifiedgarbage/format/FormatUtils.java
@@ -3,4 +3,17 @@ package ch.hiddenalpha.unspecifiedgarbage.format;
 
 /** Obsolete: This class did migrate to "xtra4j" */
 @Deprecated
-public class FormatUtils {}
+public class FormatUtils {
+
+    /* TODO should we move this to xtra4j? */
+    public static String getAsHexStr( byte[] src, int srcOff, int len ){
+        byte[] tmp = new byte[len * 2];
+        for( int i = 0 ; i < len ; ++i ){
+            int lo = src[srcOff + i] & 0x0F, hi = (src[srcOff + i] >>> 4) & 0x0F;
+            tmp[2 * i] = (byte)( (hi <= 9) ? hi + '0' : hi + 'A' - 10 );
+            tmp[2 * i + 1] = (byte)( (lo <= 9) ? lo + '0' : lo + 'A' - 10 );
+        }
+        return new String(tmp, US_ASCII);
+    }
+
+}
