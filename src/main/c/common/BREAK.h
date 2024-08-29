8695bd6715721e7fa8d0ce0f65113e77dc9c6fcc
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2024-08-29 02:17:40 +0200

    Fix -DBREAK override.

--- src/main/c/common/BREAK.h
+++ src/main/c/common/BREAK.h
@@ -1,9 +1,9 @@
 
-/* "-DBREAK=do{assert(0);abort();}while(0)" */
+/* -DBREAK\(\)=abort\(\) */
 #ifndef BREAK
 #    define BREAK() do{ \
         LOGDBG("SIGTRAP %s:%d\n", __FILE__, __LINE__); \
-        __asm__("int $3; nop"); \
+        __asm__("int $3; nop"); abort(); \
     }while(0)
 #endif
 
