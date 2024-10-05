e7cc8eafa03d975a6c6603cb5f143c68d1fcdd2b
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2024-10-05 12:36:41 +0200

    Cleanup container_of.h for even easier CopyPasta

--- src/main/c/common/offset_of.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCGUARD_yisgKqALPG4lfEqb
-#define INCGUARD_yisgKqALPG4lfEqb
-
-
-#define container_of(P, T, M) \
-    ((T*)( ((size_t)P) - ((size_t)((char*)&((T*)0)->M - (char*)0) )))
-
-
-#endif /* INCGUARD_yisgKqALPG4lfEqb */
