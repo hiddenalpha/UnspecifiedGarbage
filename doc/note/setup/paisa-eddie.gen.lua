aae72b8337c494cedbbf7d466b1e22733bb401bb
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2024-12-17 16:24:09 +0100

    Update notes about eddieVM, ssh.

--- doc/note/setup/paisa-eddie.gen.lua
+++ doc/note/setup/paisa-eddie.gen.lua
@@ -11,7 +11,9 @@
 -- Customize your setup here: -------------------------------------------------
 
 local env_PROXY_URL = "http://10.0.2.2:3128/"
-local env_PROXY_NO  = "127.0.0.1,10.0.2.*"
+local env_PROXY_NO  = "127.0.0.1,localhost,10.0.2.2,10.0.2.15"
+local env_PAISA_ENV = "test"
+-- keys on fedora/RHEL MUST be at least 3072 bits.
 local isaSshPubKey = nil
 
 -- EndOf Customizations -------------------------------------------------------
@@ -21,6 +23,7 @@ local main
 
 function main()
     local dst = io.stdout
+    assert(not(isaSshPubKey:find("'")))
     dst:write("#!/bin/sh\nset -e\n")
     dst:write([=[true \
   && SUDO=sudo \
@@ -37,6 +40,9 @@ function main()
   && $SUDO dnf remove -y firewalld \
   && $SUDO systemctl enable sshd \
   && $SUDO systemctl start sshd \
+  && printf %s\\n \
+       "export PAISA_ENV=]=].. env_PAISA_ENV ..[=[" \
+     | $SUDO tee -a /etc/profile >/dev/null \
   && if test -n "${PROXY_URL?}"; then true \
       && printf %s\\n \
            "export http_proxy=\"${PROXY_URL:?}\"" \
@@ -50,33 +56,60 @@ function main()
   && $SUDO mkdir -p /home/isa \
   && $SUDO chown ${isaUid:?}:${appGid:?} /home/isa \
   && $SUDO mkdir -p /data/instances \
+  && $SUDO mkdir -p /mnt/data/fenchurch \
+  && $SUDO chown root:app /mnt/data/fenchurch \
+  && $SUDO chmod 775 /mnt/data/fenchurch \
   && $SUDO chgrp ${appGid:?} /data/instances \
   && $SUDO chmod 775 /data/instances \
   && mkdir -p /home/isa/.ssh \
-  && printf %s ']=].. isaSshPubKey ..[=[' >> /home/isa/.ssh/authorized_keys \
+  && printf %s\\n ']=].. isaSshPubKey ..[=[' >> /home/isa/.ssh/authorized_keys \
+  && find /home/isa/.ssh $SUDO chown ${isaUid:?}:${isaGid:?} /home/isa/.ssh \
+  && find /home/isa/.ssh -type d $SUDO chmod 700 /home/isa/.ssh \
+  && find /home/isa/.ssh -type f $SUDO chmod 600 /home/isa/.ssh \
   && `# Inject some inlien documentation ` \
   && printf %s\\n \
          '' \
-         '## Additional packages, helpful for some debugging scenarios' \
+         '  TODO: setup *isa-docker* network' \
+         '  TODO: setup *isa-vehicle* network' \
+         '' \
+         '  ## Additional packages, helpful for some debugging scenarios' \
          '' \
          '  && $SUDO dnf install -y java-11-openjdk-devel.x86_64' \
          '  && $SUDO dnf install -y nss-mdns avahi avahi-tools' \
-         '  && printf "\n  DONE :)\n\n"' \
          '' \
      | $SUDO tee /home/isa/README.txt >/dev/null \
   && $SUDO chown ${isaUid:?}:${isaGid:?} /home/isa/README.txt \
+  && `# [docker networking](https://gitit.post.ch/projects/ISA/repos/wowbagger-puppetconfig/browse/site/profile/templates/container-networking-setup.sh.erb) ` \
+  && $SUDO podman network create \
+         --driver=bridge \
+         --subnet=192.168.198.0/25 \
+         `# --opt com.docker.network.driver.mtu=<%= @mtu %> ` \
+         `# --opt com.docker.network.bridge.enable_icc=true ` \
+         `# --opt com.docker.network.bridge.enable_ip_masquerade=false ` \
+         `# --opt com.docker.network.bridge.host_binding_ipv4=0.0.0.0 ` \
+         --opt "com.docker.network.bridge.name=isa-docker" \
+         isa-docker \
+  && $SUDO podman network create \
+         --driver=macvlan \
+         --subnet=192.168.10.0/24 \
+         --gateway=192.168.10.1 \
+         --ip-range=192.168.10.8/29 \
+         --opt parent=ens3 \
+         isa-vehicle \
   && `# Force-replace isa related users/groups. ` \
+  && $SUDO sed -i -E 's/^(%wheel\s+ALL=\(ALL\)\s+)(ALL\s*)$/\1NOPASSWD:\2/' /etc/sudoers \
+  && $SUDO mv /etc/sudoers-FBsAAJpmAAA5ewAA /etc/sudoers \
   && <<EOF $SUDO sh - &&
 true \
-  `# && cat /etc/passwd ` \
-  `#    | grep -v -E '^isa:' ` \
-  `#    | grep -v -E '^[^:]*:[^:]*:'${isaUid:?}':' ` \
-  `#    | tee /etc/passwd-9D8AAONcAADpbAAA >/dev/null ` \
-  `# && cat /etc/group ` \
-  `#    | grep -v -E '^app:' ` \
-  `#    | grep -v -E '^[^:]*:[^:]*:'${appGid:?}':' ` \
-  `#    | tee /etc/group-Y2MAAOIIAABeCQAA >/dev/null ` \
-  `# && mv /etc/group-Y2MAAOIIAABeCQAA /etc/passwd ` \
+  && cat /etc/passwd \
+     | grep -v -E '^isa:' \
+     | grep -v -E '^[^:]*:[^:]*:'${isaUid:?}':' \
+     | tee /etc/passwd-9D8AAONcAADpbAAA >/dev/null \
+  && cat /etc/group \
+     | grep -v -E '^app:' \
+     | grep -v -E '^[^:]*:[^:]*:'${appGid:?}':' \
+     | tee /etc/group-Y2MAAOIIAABeCQAA >/dev/null \
+  && mv /etc/group-Y2MAAOIIAABeCQAA /etc/passwd \
   && printf "isa:x:${isaUid:?}:${isaGid:?}:isa:/home/isa:/bin/bash\n" | tee -a /etc/passwd >/dev/null \
   && printf "isa:x:${isaUid:?}:\n" | tee -a /etc/group >/dev/null \
   && cat /etc/group \
@@ -86,6 +119,7 @@ true \
   && printf %s\\n \
        "app:x:${appGid:?}:isa" \
      | tee -a /etc/group >/dev/null \
+  && echo 'isa:12345' | $SUDO chpasswd \
   && true
 EOF
 true \
